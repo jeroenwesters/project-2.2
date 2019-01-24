dfeb287fb452eb88a14bffe4dba6f27422cdf467
Jarco Uil <jarcouil@gmail.com>
2019-01-24 19:31:26 +0100

    Update register.php
    Actually registers the user in the database now.

--- Client/register.php
+++ Client/register.php
@@ -1,14 +1,42 @@
 <?php
-
+  require "include/header.php";
   if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST))
   {
     $username = $_POST['username'];
     $password0 = $_POST['password0'];
     $password1 = $_POST['password1'];
-    if(isset($POST['admin'])){
-      $admin = $_POST['admin'];
+    if(isset($_POST['admin'])){
+      $admin = 1;
+    }
+    else{
+      $admin = 0;
+    }
+    $error = "";
+    if(strlen($password0) < 5){
+      $error = "The password should have at least 5 characters.";
+    }
+    if($password0 != $password1){
+      $error = "The passwords do not match.";
+    }
+    if(strlen($error) > 0){
+      echo $error;
+    }
+    else{
+      $query = "INSERT INTO `users` (`userid`, `username`, `password`, `admin`)
+              VALUES (NULL, '$username', '$password0', '$admin')";
+      $result = doQuery($conn, $query);
+      $sqlerror = $conn->error;
+      if(strlen($sqlerror) < 1){
+        echo "Registratie voltooid";
+      }
+      if(strtok($sqlerror, " ") == "Duplicate"){
+        echo "This username has already been taken";
+      }
+      else{
+        echo "An sql error has occured:";
+        echo $sqlerror;
+      }
     }
   }
-require "admin.php"
-
+  echo "<br><a href='admin.php'>Terug naar de admin pagina";
  ?>
