24724b0134086aa9e7d91465e0070a286f1e0a11
jeroen <jeroenwesters@hotmail.com>
2019-02-04 20:26:47 +0100

    [Cleanup] Removed files

--- Client/menu.php
+++
@@ -1,9 +0,0 @@
-<div class="left">
-  <a href="webpage.php">Home</a>
-  <br>
-  <a href="view1.php">view1</a>
-  <br>
-  <a href="view2.php">view2</a>
-  <br>
-  <a href="view3.php">view3</a>
-</div>
