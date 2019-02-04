d68c2ae71ea4c84200dfad197a36be0aba6bd80c
jeroen <jeroenwesters@hotmail.com>
2019-02-04 20:25:27 +0100

    [Fix] removed files [Add] design to register.php

--- Client/register.php
+++ Client/register.php
@@ -1,48 +1,60 @@
 <?php
-  require 'include/layout/headersettings.php';
-  require 'include/functions.php';
-  if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST))
-  {
-    $username = $_POST['username'];
-    $password0 = $_POST['password0'];
-    $password1 = $_POST['password1'];
-    if(isset($_POST['admin'])){
-      $admin = 1;
-    }
-    else{
-      $admin = 0;
+// Made by Jarco - © 2019
+
+// Load settings!
+require 'include/layout/headersettings.php';
+require 'include/functions.php';
+
+// Create settings
+$headerSettings = new HeaderSettings();
+$headerSettings->AddStyle("style/main.css");
+$headerSettings->title = 'Succesfully registerd!';
+
+require 'include/layout/header.php';
+require 'include/layout/navbar.php';
+?>
+
+<!-- Content  -->
+<div class="maindiv">
+
+
+    <?php
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST))
+    {
+      $username = $_POST['username'];
+      $password0 = $_POST['password0'];
+      $password1 = $_POST['password1'];
+      if(isset($_POST['admin'])){
+        $admin = 1;
+      }
+      else{
+        $admin = 0;
+      }
+
+      $result = createAccount($username, $password0, $password1, $admin);
+
+      if($result->error){
+        echo '<br><br><br>';
+        echo '<h1>Failed!</h1>';
+        echo '<p1>'.$result->message.'</p1>';
+
+      }else{
+        echo '<br><br><br>';
+        echo '<h1>Registerd!</h1>';
+        echo '<p1>Username: '.$username.'</p1>';
+      }
+
     }
 
-    $result = createAccount($username, $password0, $password1, $admin);
+    echo '<div class="center-box" id="loading">';
+    echo '<br><button class="button" onclick="goBack()">Go Back</button>';
+    echo '<script>function goBack() { window.history.back(); }</script>';
 
-    echo $result->message;
-    // $error = "";
-    // if(strlen($password0) < 5){
-    //   $error = "The password should have at least 5 characters.";
-    // }
-    // if($password0 != $password1){
-    //   $error = "The passwords do not match.";
-    // }
-    // if(strlen($error) > 0){
-    //   echo $error;
-    // }
-    // else{
-    //   $hashedPassword = password_hash($password0, PASSWORD_BCRYPT);
-    //   $query = "INSERT INTO `users` (`userid`, `username`, `password`, `admin`)
-    //           VALUES (NULL, '$username', '$hashedPassword', '$admin')";
-    //   $result = doQuery($conn, $query);
-    //   $sqlerror = $conn->error;
-    //   if(strlen($sqlerror) < 1){
-    //     echo "Registration complete";
-    //   }
-    //   else if(strtok($sqlerror, " ") == "Duplicate"){
-    //     echo "This username has already been taken";
-    //   }
-    //   else{
-    //     echo "An sql error has occured:";
-    //     echo $sqlerror;
-    //   }
-    // }
-  }
-  echo "<br><a href='admin.php'>Back to the admin panel";
- ?>
+
+    ?>
+
+  </div>
+</div>
+
+<?php include 'include/layout/footer.php';  ?>
